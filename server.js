0473d1dd6ba3e6cf67fdd15a83c7f750f18a402d
Ariel-Joe <arieljoe@gmail.com>
2024-12-03 15:28:56 +0300

    frontend for the app

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-import express from "express"
+import { config } from "dotenv";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { getHOME, patchHOME, postHOME, } from "./controllers/home.js"
 import {getToDo, createToDo} from "./controllers/todo.js"
+import mongoose from "mongoose";
 const app = express()
 
+config()
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.route("/").get(getHOME).post(postHOME).patch(patchHOME);
 
 app.route("/todo").get(getToDo).post(createToDo);
 
-app.listen(3200, () => {
-    console.log("server is running on http://localhost:3200")
-}) 
+const PORT = process.env.PORT || 3200;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
